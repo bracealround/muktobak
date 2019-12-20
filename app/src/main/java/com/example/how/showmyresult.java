49d1735e3fc3d283f3fb656f47793bc350177c5d
abudullah <abdullahbinkhaleds@gmail.com>
2019-12-21 00:39:14 +0600

    "Paisos?"

---
+++ app/src/main/java/com/example/how/showmyresult.java
@@ -0,0 +1,14 @@
+package com.example.how;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showmyresult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showmyresult);
+    }
+}
